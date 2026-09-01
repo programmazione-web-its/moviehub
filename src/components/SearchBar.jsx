53e18f66904f1a77bd5bcce5aa98bdeb6248a82e
analayze <annalisa@antonioli.dev>
2026-09-01 16:04:25 +0200

    Add focus functionality to SearchBar component; utilize useRef and useEffect hooks

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,17 @@
+import { useRef, useEffect } from 'react'
+
 export default function SearchBar({ inputValue, onChange }) {
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    if (!inputRef?.current) return
+    inputRef.current.focus()
+  }, [])
+
   return (
     <div className='search-bar-wrapper'>
       <input
+        ref={inputRef}
         type='text'
         value={inputValue}
         onChange={(e) => onChange(e.target.value)}
